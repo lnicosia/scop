2f0d62ca70f9b8bb2f8b949944f29235af18b5a4
lnicosia <lnicosia@student.42.fr>
2021-01-21 14:10:38 +0100

    [ADD] Can move the object in the 4 directions.

--- includes/inputs.h
+++ includes/inputs.h
@@ -6,26 +6,30 @@
 /*   By: lnicosia <lnicosia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/10 17:58:37 by lnicosia          #+#    #+#             */
-/*   Updated: 2021/01/21 13:58:57 by lnicosia         ###   ########.fr       */
+/*   Updated: 2021/01/21 14:04:43 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INPUTS_H
 # define INPUTS_H
-# define MAX_INPUTS 3
+# define MAX_INPUTS 7
 
 enum				e_inputs
 {
 	ESCAPE,
 	ENTER,
 	DRAW_MODE,
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN,
 };
 
 enum				e_input_states
 {
 	PRESSED,
 	RELEASED,
-	UP,
+	REST,
 };
 
 typedef struct		s_input
--- src/inputs.c
+++ src/inputs.c
@@ -6,11 +6,11 @@
 /*   By: lnicosia <lnicosia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 12:58:23 by lnicosia          #+#    #+#             */
-/*   Updated: 2021/01/21 14:01:50 by lnicosia         ###   ########.fr       */
+/*   Updated: 2021/01/21 14:10:00 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "env.h"
+#include "scop.h"
 #include "libft.h"
 #include "inputs.h"
 
@@ -21,27 +21,34 @@ int		init_inputs(t_input *inputs)
 	i = 0;
 	while (i < MAX_INPUTS)
 	{
-		inputs[i].key1 = -1;
-		inputs[i].key2 = -1;
-		inputs[i].state = UP;
+		inputs[i].key1 = GLFW_KEY_UNKNOWN;
+		inputs[i].key2 = GLFW_KEY_UNKNOWN;
+		inputs[i].state = REST;
 		i++;
 	}	
 	inputs[DRAW_MODE].key1 = GLFW_KEY_M;
-	inputs[DRAW_MODE].key2 = -1;
 	inputs[ESCAPE].key1 = GLFW_KEY_ESCAPE;
+	inputs[LEFT].key1 = GLFW_KEY_A;
+	inputs[LEFT].key2 = GLFW_KEY_LEFT;
+	inputs[RIGHT].key1 = GLFW_KEY_D;
+	inputs[RIGHT].key2 = GLFW_KEY_RIGHT;
+	inputs[UP].key1 = GLFW_KEY_W;
+	inputs[UP].key2 = GLFW_KEY_UP;
+	inputs[DOWN].key1 = GLFW_KEY_S;
+	inputs[DOWN].key2 = GLFW_KEY_DOWN;
 	return (0);
 }
 
 enum e_input_states		update_input_state(t_input input, t_env *env)
 {
-	if ((input.key1 != -1 &&
+	if ((input.key1 != GLFW_KEY_UNKNOWN &&
 		glfwGetKey(env->window, input.key1) == GLFW_PRESS)
-		|| (input.key2 != -1
+		|| (input.key2 != GLFW_KEY_UNKNOWN
 		&& glfwGetKey(env->window, input.key2) == GLFW_PRESS))
 		return (PRESSED);
 	if (input.state == PRESSED)
 		return (RELEASED);
-	return (UP);
+	return (REST);
 }
 
 int		update_inputs(t_input *inputs, t_env *env)
@@ -64,6 +71,22 @@ int		process_inputs(t_input *inputs, t_env *env)
 	{
 		ft_printf("Draw mode swapped\n");
 		env->polygon_mode = env->polygon_mode == GL_LINE ? GL_FILL : GL_LINE;
+	}
+	if (inputs[LEFT].state == PRESSED)
+	{
+		move_object(&env->objects[0].instances[0], new_v3(-0.01f, 0.0f, 0.0f));
+	}
+	if (inputs[RIGHT].state == PRESSED)
+	{
+		move_object(&env->objects[0].instances[0], new_v3(0.01f, 0.0f, 0.0f));
+	}
+	if (inputs[UP].state == PRESSED)
+	{
+		move_object(&env->objects[0].instances[0], new_v3(0.0f, 0.01f, 0.0f));
+	}
+	if (inputs[DOWN].state == PRESSED)
+	{
+		move_object(&env->objects[0].instances[0], new_v3(0.0f, -0.01f, 0.0f));
 	}
 	return (0);
 }
